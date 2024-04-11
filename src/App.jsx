5b557d317513eeb3f6c57005f8093d1ea3ed1680
Bhagyashree <khairnarb101@gmail.com>
2024-04-11 15:06:12 +0530

    GameBoard update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import Player from "./component/Player.jsx";
+import Log from "./component/Log.jsx";
 import GameBoard from "./component/GameBoard.jsx";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([])
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function hanndleSelectSquare() {
+    setActivePlayer((currentActivePlayer) =>
+      currentActivePlayer === "X" ? "O" : "X"
+    );
+    setGameTurns()
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="player">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelctSquare={hanndleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
+      <Log />
     </main>
   );
 }
